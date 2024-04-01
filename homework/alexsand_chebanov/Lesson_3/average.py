265c9e8787691fe17a246a53897688ec405ad199
ChebanovAlexsandr <chebanov.al23@yandex.ru>
2024-04-02 00:27:00 +0300

    Домашнее задание 3 урок

---
+++ homework/alexsand_chebanov/Lesson_3/average.py
@@ -0,0 +1,7 @@
+import math
+
+x = 2
+y = 6
+print((y + x) / 2)
+print(math.sqrt(abs(y) * abs(x)))
+
---
+++ homework/alexsand_chebanov/Lesson_3/hypotenuse.py
@@ -0,0 +1,7 @@
+import math
+
+a = 5.4
+b = 2.1
+print(math.sqrt(b ** 2 + a ** 2))
+print("S = " + str((a * b) / 2))
+
