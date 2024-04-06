da8ddc0e696a0dc321e1b8809543ec4f93c264f4
ChebanovAlexsandr <chebanov.al23@yandex.ru>
2024-04-06 21:39:29 +0300

    исправлено задание 2

--- homework/alexsand_chebanov/Lesson_5_chebanovau/Lesson_5_chebanov.py
+++ homework/alexsand_chebanov/Lesson_5_chebanovau/Lesson_5_chebanov.py
@@ -4,19 +4,18 @@ person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 
 name, last_name, city, phone, country = person
 
-
 # Задание 2
 string_1 = "результат операции: 42"
-result_1 = string_1.index("4")
-print(f'{int(string_1[int(result_1):]) + 10} ')
+result_1 = string_1.index(":")
+print(int(string_1[result_1 + 2::]) + 10)
 
 string_2 = "результат операции: 514"
-result_2 = string_2.index("5")
-print(f'{int(string_2[int(result_2):]) + 10} ')
+result_2 = string_2.index(":")
+print(int(string_2[result_2 + 2::]) + 10)
 
 string_3 = "результат работы программы: 9"
-result_3 = string_3.index("9")
-print(f'{int(string_3[int(result_3)]) + 10} ')
+result_3 = string_3.index(":")
+print(int(string_3[result_3 + 2::]) + 10)
 
 # Задание 3
 
@@ -24,3 +23,4 @@ students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
 print(f'Students {", ".join(students)} study these subjects: {", ".join(subjects)}')
+
