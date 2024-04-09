c2bcd6b9bf54506c2cbe96922ba5fe74a92d09d8
ChebanovAlexsandr <chebanov.al23@yandex.ru>
2024-04-09 20:44:15 +0300

    исправлен if bonus == True в коде задание 1

--- homework/alexsand_chebanov/Lesson_8_chebanovau/Lesson_8_1_chebanov.py
+++ homework/alexsand_chebanov/Lesson_8_chebanovau/Lesson_8_1_chebanov.py
@@ -6,7 +6,7 @@ def zarplata():
     salary = int(input("Введи свою зарплату: "))
     bonus = random.choice([True, False])
     value_bonus = random.randrange(100, 1000)
-    if bonus == True:
+    if bonus:
         print(f"${str(salary + value_bonus)}")
     else:
         print(salary)
