c20ea4a7617ab70a613deddc2928f40216102bda
ChebanovAlexsandr <chebanov.al23@yandex.ru>
2024-04-11 21:53:34 +0300

    Исправленое задание 8.2.Убран список,обращение через цикл

--- homework/alexsand_chebanov/Lesson_8_chebanovau/Lesson_8_2_chebanovau.py
+++ homework/alexsand_chebanov/Lesson_8_chebanovau/Lesson_8_2_chebanovau.py
@@ -1,21 +1,28 @@
 import sys
 
 # Задание 2
-sys.set_int_max_str_digits(30000)
-
 count = 0
-value = 100000
+sys.set_int_max_str_digits(50000)
 
 
-def fib(n):
-    a, b = 0, 1
-    for _ in range(n):
+def fibonacci_generator():
+    a = 1
+    b = 1
+    while True:
         yield a
-        a, b = b, a + b
+        a = b
+        b = a + b
 
 
-g = list(fib(value))
-for i in g:
-    if i == g[4] or i == g[199] or i == g[999] or i == g[99999]:
-        count += 1
-        print(f'{count}){i}\n')
+for num in fibonacci_generator():
+    if count == 5:
+        print(f'Пятое число:{num}')
+    if count == 200:
+        print(f'Двухсотое число:{num}')
+    if count == 1000:
+        print(f'Тысячное число:{num}')
+    if count == 100000:
+        print(f'Стотысячное число:{num}')
+        break
+
+    count += 1
